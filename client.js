b8f06a0062406209b3e28616854887d81e69f852
Andrew On <a_on93@hotmail.com>
2023-09-22 10:15:42 -0400

    'updated functions in input.js'

--- client.js
+++ client.js
@@ -4,15 +4,16 @@ const connect = function () {
     host: "localHost",
     port: 50541,
   });
-
   conn.on('connect', (connect) => {
     console.log('Connection established');
+  });
+  conn.on('connect', () => {
     conn.write(`Name: AON`);
+});
    // conn.write(`Move: up`);
     conn.on('data', (data) => {
       console.log('Server says: ', data);
       console.log("Idled too long so you have been disconnected");
-    });
   });
   // interpret incoming data as text
 
--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const { connect} = require('./client');
+const { setupInput } = require('./input');
 const net = require("net");
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (key === '\u0003') {
-    process.exit();
-  };
-
-  stdin.on('data', (data) => {
-    console.log("Exiting program");
-  });// your code here
-};
+setupInput();
